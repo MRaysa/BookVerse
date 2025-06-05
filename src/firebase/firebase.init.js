db2d1534115ce719ddcae5e66abce3b78b651cae
MRaysa <147266032+MRaysa@users.noreply.github.com>
2025-06-05 13:54:51 +0600

    added corrrectly cinfiguration

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API,
   authDomain: import.meta.env.VITE_FIREBASE,
@@ -15,9 +12,10 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// console.log("API Key:", import.meta.env.VITE_API_KEY);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const storage = getStorage(app);
+const db = getFirestore(app);
 
-// Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
+export { app, auth, storage, db };
