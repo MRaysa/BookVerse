aa5ab380bce3e9fb75bd9209f1e3f43a56d54484
MRaysa <147266032+MRaysa@users.noreply.github.com>
2025-06-17 01:16:59 +0600

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -88,7 +88,7 @@ const Navbar = () => {
       animate={{ y: 0 }}
       transition={{ type: "spring", stiffness: 100 }}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-2">
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
           <motion.div
